298582e8985ae9baffdb84e9be2c7ec8161c5f9c
laborba <lucasadeborba@gmail.com>
2024-03-05 14:24:07 -0300

    inicio do projeto

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]      //Sempre que fizer uma operação post para este controlador, irá executar o que estiver abaixo
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+    }
+}
