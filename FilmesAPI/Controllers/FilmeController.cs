3cf44d7eef3e78fbaa625ebc596eeaf2557c23d6
laborba <lucasadeborba@gmail.com>
2024-03-06 14:10:01 -0300

    acrescentado post e 2 gets

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,10 +9,28 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
 
     [HttpPost]      //Sempre que fizer uma operação post para este controlador, irá executar o que estiver abaixo
-    public void AdicionaFilme([FromBody] Filme filme)
+    public void AdicionaFilme([FromBody] Filme filme)       //[FromBody] = indica da onde pegará a informação (do corpo)
     {
+        filme.Id = id++;
         filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]       // para diferenciar o get, informamos que precisamos receber
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,8 +1,16 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]      // Required impõe que o campo é obrigatório
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caractéres.")]   // Max Length define o máximo de caractéres
     public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatório.")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 a 600 minutos.")]       // Range define um mínimo e máximo
     public int Duracao { get; set; }
 }
