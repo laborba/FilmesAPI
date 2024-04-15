91545a9f472074e1f9034e345e7da07d3f484a55
laborba <lucasadeborba@gmail.com>
2024-04-15 15:46:53 -0300

    acrescentado sistema 1:N, criado Sessao com Controller, Dtos, Model, Profile. Feito migracao e enviado ao banco

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -15,4 +15,5 @@ public class Filme
     [Required(ErrorMessage = "A duração do filme é obrigatório.")]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 a 600 minutos.")]       // Range define um mínimo e máximo
     public int Duracao { get; set; }
+    public virtual ICollection<Sessao> Sessoes { get; set; }
 }
