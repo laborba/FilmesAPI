3f1b6a124235d05f3dc12d12d45ef00f11cd94da
laborba <lucasadeborba@gmail.com>
2024-04-15 14:59:11 -0300

    criado relacao 1:1 entre endereco e cinema, feito migracao e enviado ao banco de dados, testes funcionando

--- FilmesAPI/Models/Endereco.cs
+++ FilmesAPI/Models/Endereco.cs
@@ -9,5 +9,6 @@ public class Endereco
     public int Id { get; set; }
     public string Logradouro { get; set; }
     public int Numero { get; set; }
+    public virtual Cinema Cinema { get; set; }
 
 }
